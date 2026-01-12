67d67ad452006aec0a1e2e5840b7a642f7a5dd67
Sanjay_Sumanas <sanjay.yadav@arkinfotec.com>
2026-01-12 11:43:33 +0530

    update DatabaseSeeder and screenshots section

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             AdminUserSeeder::class,
+            PermissionSeeder::class,
         ]);
     }
 }
